3fceb1a41dcdbb7521eb5109474f6fdb3cf05b70
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-02 21:06:32 -0700

    First fix for 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	static const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	fwrite(str, sizeof(str) - 1, 1, stdout);
 	return (1);
 }
