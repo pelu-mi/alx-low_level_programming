2ced3dd73bfb72c063d97c3a4c05c9b246e8087d
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-03 18:10:54 -0700

    Added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+	for(i = 0; i < 26; i++)
+		putchar(str[i]);
+	putchar('\n');
+
+	return (0);
+}
