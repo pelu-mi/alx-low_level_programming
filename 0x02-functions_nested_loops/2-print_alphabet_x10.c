9352c9b042ac60e53c6006ca21eaf1bc6695b7bc
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-07 16:07:50 -0700

    Added 2-print_alphabet_x10 and updated main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the lower case alphabets 10 times
+ * Return: Void
+ */
+void print_alphabet_x10(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz\n"
+	int i = 0, j = 0;
+
+	while (i < 10)
+	{
+		while (j < 27)
+		{
+			_putchar(str[j]);
+			j++;
+		}
+		i++;
+	}
+}
