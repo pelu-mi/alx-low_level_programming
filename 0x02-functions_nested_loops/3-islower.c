8827c7903416aa5561d36b6346cc4003067a6cf7
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-07 16:34:48 -0700

    First fix for 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,17 @@
 
 /**
  * _islower - Check if input is a lower case character
+ * @c: character input to check if it is lower case
  * Return: 1 if yes, 0 if no
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
+	{
 		return 1;
+	}
 	else
+	{
 		return 0;
+	}
 }
