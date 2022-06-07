9100edbdb6958ab6f3db03b8ab898f1a98549475
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-07 16:59:00 -0700

    Added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Check if input is a lower case character
+ * @n: number input to check if it is positive
+ * Return: 1 if positive, 0 if zero and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
