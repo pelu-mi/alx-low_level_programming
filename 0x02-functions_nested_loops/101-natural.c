50969c90bedbcb03ac763b242b368847c4cd8164
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-07 20:05:32 -0700

    Added 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Sum all multiples of 3 and 5 between 1 and 1024
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 0, sum = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum+=5;
+		i++;
+	}
+	return (0);
+}
