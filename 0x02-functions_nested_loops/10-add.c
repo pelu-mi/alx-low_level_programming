5464fd05fcd11fa4bb7306080a9ea0df8b5bfabd
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-07 18:44:03 -0700

    Added 10-add.c and updated main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Return sum of two numbers
+ * @a: number to add
+ * @b: number to add
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
