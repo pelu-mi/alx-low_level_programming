265958a558009cf04cd7d00f9533faa0451dcb9b
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-07 18:33:57 -0700

    Third Fix for 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,27 @@
  */
 void times_table(void)
 {
-	int i, j, a;
+	int i, j, a, mod, lead;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			a = i * j;
-			if (a < 9 && a !=0)
+			if (a < 10 && a !=0)
+			{
 				_putchar(' ');
-			_putchar(a + '0');
+				_putchar(a + '0');
+			else if (a > 9)
+			{
+				mod = a % 10;
+				lead = (a % 100) / 10;
+				_putchar(lead + '0');
+				_putchar(mod + '0');
+			}
+			else if (a == 0)
+				_putchar(a + '0');
+
 			if (j == 9)
 			{
 				_putchar('\n');
