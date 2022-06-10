bbd2c10a5a895e98f75b638dbc6dd40de42b90d8
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-09 21:15:18 -0700

    Added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Fix FizzBuzz problem
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+}
