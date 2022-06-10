3e8a33c952329141b7b547b613489e358001195e
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-09 21:26:57 -0700

    Added 10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print triangle with height of size
+ * @size: height of triangle
+ * Return: Nothing
+ */
+void print_triangle(int size)
+{
+	int i = 0, j;
+
+	if (size > 0)
+	{
+		while (i < size)
+		{
+			j = i;
+			while (j > 0)
+			{
+				_putchar(' ');
+				j--;
+			}
+			k = 0;
+			while (k < i)
+			{
+				_putchar('#');
+				k++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+		_putchar('\n');
+}
