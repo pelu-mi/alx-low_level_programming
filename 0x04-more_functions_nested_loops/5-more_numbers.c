237e1a50098612203c686a4f43220883f71bc143
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-09 20:16:59 -0700

    Added 5-more_numbers.c and updated main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print numbers 0 to 14, 10 times
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
