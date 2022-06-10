1f7d4c5959b197ff28273566cb4703edd069dafb
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-09 21:47:14 -0700

    Added 101-print-number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - print any integer n
+ *@n: integer to print
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	} else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+
+	_putchar((num % 10) + '0');
+}
