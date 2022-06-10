7e0abfee87e87b266f95797dc9f5bc2bf9204553
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-09 19:57:01 -0700

    Added 3-print_numbers and updated main.h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers 0 to 9
+ * Return: Nothing
+ */
+void print_numbers(void);
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++
+	}
+	_putchar('\n');
+}
