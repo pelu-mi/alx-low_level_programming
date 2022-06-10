80cea44af76ae30f8f7e29f9d78540f6964355f6
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-09 19:30:53 -0700

    Second fix for 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - Function to check if a character is upper case or not
- * @c: Ascii code for character
- * Return: 1 if upper case, 0 if not
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
