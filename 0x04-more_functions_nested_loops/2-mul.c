742e2557115d3b16ad7760ed222d4a4577ea401a
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-09 19:50:51 -0700

    Added 2-mul.c and updated main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - returns product of a and b
+ * @a: number to be multiplied
+ * @b: number to be multiplied
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
