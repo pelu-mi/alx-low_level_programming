a25469fbd43abde05be8d619e5e4fc3f80a1d922
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-13 20:36:41 -0700

    Added 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an array
+ * @a: array pointer to use
+ * @n: number of elements to print
+ * Return: Nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", *(a + i) + '0');
+		if (i != n - 1)
+			printf(", ");
+		i++;
+	}
+	_putchar('\n');
+}
