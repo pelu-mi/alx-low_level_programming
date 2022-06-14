09ff93fba4fc361531e0186a86b9f4711a6e4ecb
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-13 18:48:32 -0700

    Added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - Find length of a String
+ * @s: Char pointer to use
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = sizeof(*s) / sizeof(*s[0]);
+	return (len);
+}
