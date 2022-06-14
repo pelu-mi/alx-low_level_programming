561e0f490716e17a68988b65677922cbe69e1b39
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-13 19:45:31 -0700

    First fix for 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,14 @@ void print_rev(char *s)
 	int i, j;
 
 	i = 0;
-	while (*(str + i) != '\0')
+	while (*(s + i) != '\0')
 	{
 		i++;
 	}
 	j = i - 1;
 	while(j >= 0)
 	{
-		_putchar(*(str + j));
+		_putchar(*(s + j));
 		j--;
 	}
 	_putchar('\n');
