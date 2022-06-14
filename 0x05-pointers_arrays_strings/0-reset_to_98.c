3fef80a499ade52f48af604547f4eca5ac41367c
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-13 18:37:04 -0700

    Added 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Change pointer variable to 98
+ * @n: Pointer with variable to be changed
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
