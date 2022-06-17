36e3c7b0e5a0ce24accd2dd7c1c98e3c6b624670
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-16 19:55:49 -0700

    Added 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverse an array
+ * @a: Array to be reversed
+ * @n: length of array
+ * Return: Nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, swap[1000];
+
+	i = 0;
+	j = n;
+	while (i < n)
+	{
+		swap[i] = *(a + j);
+		i++;
+		j--;
+	}
+
+	while (j < n)
+	{
+		*(a + j) = swap[i];
+		i--;
+		j++;
+	}
+}
