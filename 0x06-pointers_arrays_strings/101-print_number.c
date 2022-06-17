5aae0548b520184b861274c9517bd658724d35b3
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-16 21:58:22 -0700

    Added 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - print any integer
+ * @n: number to print
+ * Return: number to print
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if (n == 0)
+		_putchar('0');
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar('0' + n % 10);
+
+}
