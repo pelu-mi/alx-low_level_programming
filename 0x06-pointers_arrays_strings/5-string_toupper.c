0ca250079dfc7e501d9dae6178874af889fa2e1e
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-16 20:09:57 -0700

    Added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - convert string to upper case
+ * @c: string to convert
+ * Return: Converted string
+ */
+
+char *string_toupper(char *c)
+{
+	int i, swap;
+
+	i = 0;
+	while (*(c + i) != '\0')
+	{
+		if (*(c + i) >= 'a' && *(c + i) <= 'z')
+			*(c + i) -= 22;
+		i++;
+	}
+	return (c);
+}
