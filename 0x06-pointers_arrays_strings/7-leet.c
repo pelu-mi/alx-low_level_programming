6c6fbf221be56b861f40ed3c5059025b03053fa2
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-16 21:20:28 -0700

    Fix 1 for 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,23 +8,21 @@
 
 char *leet(char *c)
 {
-	int i;
+	int i, j;
+	char find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char replace[] = {'4', '3', '0', '7', '1'};
 
-	i = 0;
-	while (*(c + i) != '\0')
+	for (i = 0; c[i] != '\0'; i++)
 	{
-		if (*(c + i) == 'a' || *(c + i) == 'A')
-			*(c + i) = '4';
-		else if (*(c + i) == 'e' || *(c + i) == 'E')
-			*(c + i) = '3';
-		else if (*(c + i) == 'o' || *(c + i) == 'O')
-			*(c + i) = '0';
-		else if (*(c + i) == 't' || *(c + i) == 'T')
-			*(c + i) = '7';
-		else if (*(c + i) == 'l' || *(c + i) == 'L')
-			*(c + i) = '1';
-
-		i++;
+		for (j = 0; j < 9; j++)
+		{
+			if (c[i] == find[j])
+			{
+				c[i] = replace[j / 2];
+				break;
+			}
+		}
 	}
+
 	return (c);
 }
