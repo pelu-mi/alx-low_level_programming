9af8fb1f241cc8af44182681490983c76ab97ac6
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-16 19:47:27 -0700

    Fourth fix for 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	int j;
 
 	j = 0;
-	while (*(s1 + j) == '\0' || *(s2 + j) == '\0')
+	while (*(s1 + j) != '\0' || *(s2 + j) != '\0')
 	{
 		if (*(s1 + j) != *(s2 + j))
 		{
