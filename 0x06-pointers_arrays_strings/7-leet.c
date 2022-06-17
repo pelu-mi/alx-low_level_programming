5e2ec3067c2f8105846786fde94ff76f97feb94e
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-16 21:33:25 -0700

    Fix 3 for 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,24 @@
 #include "main.h"
-
-/**
- * leet - convert string to 1337
- * @c: string to convert
- * Return: Converted string
- */
+/**                                                                                                                                                                   
+ * leet - convert string to 1337                                                                                                                                      
+ * @c: string to convert                                                                                                                                              
+ * Return: Converted string                                                                                                                                           
+ */  
 
 char *leet(char *c)
 {
 	int i, j;
-	char find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	char replace[] = {'4', '3', '0', '7', '1'};
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replace[] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			if (c[i] == find[j])
 			{
 				c[i] = replace[j / 2];
-				j = 9;
+				break;
 			}
 		}
 	}
