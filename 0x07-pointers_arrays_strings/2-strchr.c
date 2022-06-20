1eb1f30967b548b53525819f443552cbe65b559c
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-20 14:48:55 -0700

    Added 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Function to find first occurence of a char
+ * @s: pointer to string to check
+ * @c: Char to check for
+ * Return: pointer to first occurence of c in s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (*(s + i) == c)
+			break;
+		i++;
+	}
+
+	return (s + i);
+}
