94ba31b656fe3dc217d02c70d8b2d00b0cf06d5d
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-20 15:26:20 -0700

    Fix 7 for task 2

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_GUARD
 #define FUNCTION_GUARD
 
+#include <stddef.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
