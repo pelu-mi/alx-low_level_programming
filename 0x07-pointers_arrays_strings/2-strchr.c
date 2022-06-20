cd9ab53124cf0577feed53fa051418d3784954a8
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-20 15:08:51 -0700

    Fix 3 for 2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,14 +12,12 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (i >= 0)
+	while (*(s + i) == '\0')
 	{
-		if (*(s + i) == '\0')
-			return (NULL);
 		if (*(s + i) == c)
-			break;
+			return (s + i);
 		i++;
 	}
 
-	return (s + i);
+	return (NULL);
 }
