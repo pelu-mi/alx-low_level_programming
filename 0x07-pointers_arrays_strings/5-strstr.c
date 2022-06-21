5c11772be34a6d8317ef8ae0c66c30a2c6e7c5d2
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-20 17:07:04 -0700

    Added file for task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strstr - Function to find needle in a haystack
+ * @haystack: String to search
+ * @needle: Substring to search for
+ * Return: Pointer to beginning of needle in haystack or NULL
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, flag = 0;
+
+	for (i = 0; *(haystack + i) != '\0'; i++)
+	{
+		for (j = 0; j >= 0; j++)
+		{
+			if (*(haystack + i + j) != *(needle + j))
+				break;
+			if (*(needle + j) == '\0')
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 1)
+			break;
+	}
+
+	if (flag == 1)
+		return (haystack + i);
+	else
+		return ('\0');
+}
