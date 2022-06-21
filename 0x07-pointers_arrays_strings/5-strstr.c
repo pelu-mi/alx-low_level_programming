e56bf463155f76ae0184bd84d15582fafca64961
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-20 18:05:50 -0700

    Fix 7 for task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,7 @@ char *_strstr(char *haystack, char *needle)
 	if (*needle == 0)
 		return (haystack);
 
-	for ( ;*haystack != '\0'; haystack++)
+	for ( ; *haystack != '\0'; haystack++)
 	{
 		char *h = haystack;
 		char *n = needle;
