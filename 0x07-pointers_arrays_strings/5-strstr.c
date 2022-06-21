14a3960e9bc0589f707f58361a6fc1bffaabb5e2
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-20 17:33:59 -0700

    Fix 1 for task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strstr - Function to find needle in a haystack
  * @haystack: String to search
@@ -10,6 +11,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j, flag = 0;
 
+	if (*needle == 0)
+		return (haystack);
+	
 	for (i = 0; *(haystack + i) != '\0'; i++)
 	{
 		for (j = 0; j >= 0; j++)
