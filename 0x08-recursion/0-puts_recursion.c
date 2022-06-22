9154245b61dbdac595ef5485c1b6dc8db01c2a53
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-22 14:53:08 -0700

    Added file for task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Puts function using recursion
+ * @s: String to print
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return ();
+	_putchar(*s);
+	_puts_recursion(*(s + 1));
+}
