e2263ed5df1c3e36c772505f4ff0895697973677
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-23 13:46:51 -0700

    Fix 5 for task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,31 +1,6 @@
 #include "main.h"
 
 /**
- * wild - helper function for wild cmp
- * @s1: String to compare
- * @s2: String to compare with containing wildcard
- * @n: index of s1 being compared
- * @m: index of s2 being compared
- * Return: same as wildcmp
- */
-int wild(char *s2, int m, char *s1, int n)
-{
-	if (*(s1 + n) == '\0')
-	{
-		if (*(s2 + m) == '*')
-			return (wild(s2, m + 1, s1, n));
-		return (*(s2 + n) == '\0');
-	}
-
-	if (*(s2 + m) == '*')
-		return (wild(s2, m, s1, n + 1) || wild(s2, m + 1, s1, n));
-	else if (*(s1 + n) == *(s2 + m))
-		return (wild(s2, m + 1, s1, n + 1));
-	return (0);
-}
-
-
-/**
  * wildcmp - Function to compare 2 strings including wild card *
  * by using recursion
  * @s1: String to compare not containing wild card
@@ -36,5 +11,22 @@ int wild(char *s2, int m, char *s1, int n)
 
 int wildcmp(char *s1, char *s2)
 {
-	return (wild(s2, 0, s1, 0));
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
