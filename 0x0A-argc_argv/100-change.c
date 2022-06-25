7db21f8005bb6717edcbab67ff3174efc97ad4b0
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-24 19:09:46 -0700

    File for task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments in command line
+ * @argv: array of arguments in command line
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int num, cents = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(*(argv + 1));
+	while (num != 0)
+	{
+		if (num >= 25)
+		{
+			num -= 25;
+			cents++;
+		}
+		else if (num >= 10)
+		{
+			num -= 10;
+			cents++;
+		}
+		else if (num >= 5)
+		{
+			num -= 5;
+			cents++;
+		}
+		else if (num >= 2)
+		{
+			num -= 2;
+			cents++;
+		}
+		else if (num >= 1)
+		{
+			num -= 1;
+			cents++;
+		}
+	}
+	printf("%d\n", cents);
+	return (0);
+}
