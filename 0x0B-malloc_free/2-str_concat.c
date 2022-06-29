b67d7e207a296c41fea9dae0de4b5b4c72e9ba90
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-28 21:25:06 -0700

    Added file for task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * str_concat - Function to concatenate 2 strings
+ * @s1: string to be concatenated to
+ * @s2: String to concatenate
+ * Return: Concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, size, len1 = 0, len2 = 0;
+	char *s;
+
+	while (*(s1 + len1) != '\0')
+		len1++;
+	while (*(s2 + len2) != '\0')
+		len2++;
+	if (s1 == NULL)
+		len1 = 0;
+	if (s2 == NULL)
+		len2 = 0;
+
+	size = len1 + len2 + 1;
+	s = malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	while (i < len1)
+	{
+		*(s + i) = *(s1 + i);
+		i++;
+	}
+	while (i < size)
+	{
+		*(s + i) = *(s2 + size - i);
+		i++;
+	}
+
+	return (s);
+}
