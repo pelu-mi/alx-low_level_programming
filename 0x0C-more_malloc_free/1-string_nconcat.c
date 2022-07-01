61d16282f56b01202adcaf749dfac7dba302af3c
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-06-30 20:50:19 -0700

    betty fix for task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,16 +32,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (j > n)
 		j = n;
-	
+
 	s = malloc(sizeof(char) * (i + j + 1));
 	if (s == NULL)
 		return (NULL);
-	
+
 	for (k = 0; k < i; k++)
 		s[k] = s1[k];
 	for (k = 0; k < j; k++)
 		s[k + i] = s2[k];
-	
+
 	s[i + j] = '\0';
 	return (s);
 }
