938f1e0e7a4937febcdf2d206bf67927be8b59ce
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-04 17:23:48 -0700

    Fix 1 for task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
-#ifndef GUARD_H
-#define GUARD_H
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new struct for dog
+ * @name: name of Dog
+ * @age: Age of dog
+ * @owner: Dog owner
+ */
 
 struct dog
 {
