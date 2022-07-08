30a8cb80bb4174efd24dc74ad1d1ff8774a7a94e
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-07 17:36:30 -0700

    Added file for task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print all numbers passed as arguments
+ * @separator: string to use as separator between values
+ * @n: number or arguments to expect
+ * Return: Nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *val;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		val = va_arg(args, char *);
+		if (val == NULL)
+			printf("(nil)");
+		else
+			printf("%s", val);
+
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
