d51ea570a2647d8f2f37ec43cfad1e940311fcb8
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-07 17:10:24 -0700

    Fix 1 for task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,14 +8,14 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum = 0;
+	unsigned int i, sum = 0;
 	va_list args;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_args(args, int);
+		sum += va_arg(args, int);
 	}
 
 	va_end(args);
