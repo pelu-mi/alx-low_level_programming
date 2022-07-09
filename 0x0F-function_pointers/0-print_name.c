e7e1f2369242a45a19994217045e73a94f36c813
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-08 18:47:06 -0700

    Added file for task 0wq and folder for Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function to print a name
+ * @name: Name to print
+ * @f: function to use to print name
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_POINTERS_GUARD
+#define FUNCTION_POINTERS_GUARD
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int _putchar (char c);
+void print_name(char *name, void (*f)(char *));
+
+
+
+#endif	
