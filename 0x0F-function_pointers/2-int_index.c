6d99c82c49f2c2311b6deae192815d93c6fb62fc
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-08 19:09:12 -0700

    Added file for task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function to find the index of element for which
+ * cmp does not return 0
+ * @array: array to check
+ * @size: size of array
+ * @cmp: function to check
+ * Return: index of array
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || cmp == NULL)
+		return;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
