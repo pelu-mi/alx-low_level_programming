4e850fef21983328598a5c63fee523e36b1d37f1
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-14 20:41:08 -0700

    Fix 1 for task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,23 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * struct list_s - Singly linked list
+ * @str: string
+ * @len: Length of string
+ * @next: pointer to next node
+ * Description - Singly linked list node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	list_t *next;
+} list_t;
+
+
+/* Functions for project */
 size_t print_list(const list_t *h);
 
 
