a6ab692752a34a4850e847d9c785a3664cbd4ce5
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-14 20:49:20 -0700

    Fix 4 for task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,14 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	if (h->next != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		i++;
-		i += print_list(h->next);
+		h = h->next;
 	}
 	return (i);
 }
