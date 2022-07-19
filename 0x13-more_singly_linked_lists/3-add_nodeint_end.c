8986b1942625c9636fc577fda497304fe125084c
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-19 14:21:40 -0700

    Fix 2 for task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *new, *last;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
@@ -22,9 +22,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new;
 	else
 	{
-		while (head)
-			head = head->next;
-		*head = new;
+		last = *head;
+		while (last->next)
+			last = last->next;
+		last->next = new;
 	}
 
 	return (new);
