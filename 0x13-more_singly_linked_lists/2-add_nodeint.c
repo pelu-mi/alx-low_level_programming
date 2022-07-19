73d14f8a7441c0a5d0535f4cc67af2e2f97d2df9
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-19 13:57:40 -0700

    Added file for task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - Function to add node to beginning of a list
+ * @head: pointer to head of list
+ * @n: value of new node
+ * Return: New list item
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t new;
+
+	new.n = n;
+	new.next = *head;
+	*head = new;
+
+	return (new);
+}
