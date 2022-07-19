b88a5a597968c0394cc1bf4f55cdd37472ee4c84
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-19 14:58:47 -0700

    Added file for task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - Function to free all nodes in a list
+ * @head: pointer to head of list
+ * Return: Nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->n);
+		head = temp;
+	}
+}
