d61a4c9a30dd22d00295015fefb67732593bc6ea
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-22 15:10:26 -0700

    Fix 3 for task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b[i] == '\0')
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	while ((b[i] == '0') || (b[i] == '1'))
 	{
 		num <<= 1;
 		num += b[i] - '0';
