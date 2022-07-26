e6ddb3785805bae5e74bc71fc1256affa461bb6a
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-25 21:10:11 -0700

    Added file for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of file to create
+ * @text_content: content to write to filename
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, text_len = 0;
+
+	while (*(text_content + text_len) != '\0')
+		text_len++;
+
+	o = open(filename, O_CREAT | O_WRONLY, 0600);
+	w = write(o, text_content, text_len);
+
+	if (o == -1 || w = -1)
+	{
+		return (-1);
+	}
+
+	close(o);
+	return (1);
+}
