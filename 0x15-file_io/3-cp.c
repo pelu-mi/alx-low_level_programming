84c4a8656d7efaf066d00bf253c839f3e5b3691b
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-25 21:49:00 -0700

    Added file for task 3'

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
