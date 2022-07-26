ae30328d7501ff3472f83cd6254cb4a70078219b
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-07-25 20:55:49 -0700

    Fix 2 for task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - Function to read a text file and prints it to the standard output
+ * read_textfile - Read a text file and print it to the standard output
  * @filename: name of file to be read
  * @letters: number of letters it should read and print
  * Return: the actual number of letters it could read and print or 0 if error
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	r = read(o, buf, letters); /* Read content of filename into buffer */
 	w = write(STDOUT_FILENO, buf, r); /* Print to Standard output */
 
-	if (o == -1 || r = -1 || w = -1 || w != r) /* Check for any errors */
+	if (o == -1 || r == -1 || w == -1 || w != r) /* Check for any errors */
 	{
 		free(buf);
 		return (0);
