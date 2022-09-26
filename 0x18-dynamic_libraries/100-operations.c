f73ef042fcf8377172182ca4f8b648ed954d67ef
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-26 09:02:17 -0700

    Handover to UTM

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int add(int a, int b);
+int sub(int a, int b);                                                                                                                                                
+int mul(int a, int b);                                                                                                                                                
+int div(int a, int b);                                                                                                                                                
+int mod(int a, int b);                                                                                                                                                
+
+/**                                                                                                                                                                   
+ * add - Add 2 numbers                                                                                                                                                
+ * @a: num 1                                                                                                                                                          
+ * @b: num 2                                                                                                                                                          
+ * Return: sum of a and b                                                                                                                                             
+ */                                                                                                                                                                   
+int add(int a, int b)                                                                                                                                                 
+{                                                                                                                                                                     
+        return (a + b);                                                                                                                                               
+}
+
+/**                                                                                                                                                                   
+ * sub - Subtract 2 numbers                                                                                                                                                
+ * @a: num 1                                                                                                                                                          
+ * @b: num 2                                                                                                                                                          
+ * Return: subtraction of a and b                                                                                                                                            
+ */                                                                                                                                               
+int add(int a, int b)                                                                                                                                                 
+{                                                                                                                                                                     
+        return (a + b);                                                                                                                                               
+}
+
+/**                                                                                                                                                                   
+ * add - Add 2 numbers                                                                                                                                                
+ * @a: num 1                                                                                                                                                          
+ * @b: num 2                                                                                                                                                          
+ * Return: sum of a and b                                                                                                                                             
+ */                                                                                                                                                                   
+int add(int a, int b)                                                                                                                                                 
+{                                                                                                                                                                     
+        return (a + b);                                                                                                                                               
+}
+
+/**                                                                                                                                                                   
+ * add - Add 2 numbers                                                                                                                                                
+ * @a: num 1                                                                                                                                                          
+ * @b: num 2                                                                                                                                                          
+ * Return: sum of a and b                                                                                                                                             
+ */                                                                                                                                                                   
+int add(int a, int b)                                                                                                                                                 
+{                                                                                                                                                                     
+        return (a + b);                                                                                                                                               
+}
+
+/**                                                                                                                                                                   
+ * add - Add 2 numbers                                                                                                                                                
+ * @a: num 1                                                                                                                                                          
+ * @b: num 2                                                                                                                                                          
+ * Return: sum of a and b                                                                                                                                             
+ */                                                                                                                                                                   
+int add(int a, int b)                                                                                                                                                 
+{                                                                                                                                                                     
+        return (a + b);                                                                                                                                               
+}
