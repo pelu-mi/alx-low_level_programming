c337e27bbc17d7f156492c6af69de044bd1da6b0
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-26 07:27:11 +0100

    Added file for task 0 and header file

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Print all doubly linked list elements
+ * @h: Doubly linked list to print out all elements
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int n = 0;
+
+	while(h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This readme is for doubly linked lists
