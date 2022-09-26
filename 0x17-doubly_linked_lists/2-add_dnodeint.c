e388f3f227eb8dada4039268062e7baa98ec6996
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-26 07:55:18 +0100

    Added files for task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Add node to the beginning of a doubly linked list
+ * @head: Doubly linked list
+ * @n: Value of new node
+ * Return: Address of new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = n;
+	node->prev = NULL;
+	node->next = *head;
+
+	*head = node;
+	return (node);
+}
