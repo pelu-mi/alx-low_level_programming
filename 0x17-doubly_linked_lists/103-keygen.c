eb8f6404cb089a4533bf8420ad2bce21223ee48c
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-26 11:56:56 +0100

    Added file for task 10

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - generate a key for each username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
+ *
+ * Return: 0 on success, 1 on error
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	;
+}
