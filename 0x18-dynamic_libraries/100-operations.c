037fc934f787fc1bea5f632bc31fe280823556e9
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-26 17:11:06 +0100

    Handover to shell terminal

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -3,62 +3,71 @@
 #include <stddef.h>
 
 int add(int a, int b);
-int sub(int a, int b);                                                                                                                                                
-int mul(int a, int b);                                                                                                                                                
-int div(int a, int b);                                                                                                                                                
-int mod(int a, int b);                                                                                                                                                
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 
-/**                                                                                                                                                                   
- * add - Add 2 numbers                                                                                                                                                
- * @a: num 1                                                                                                                                                          
- * @b: num 2                                                                                                                                                          
- * Return: sum of a and b                                                                                                                                             
- */                                                                                                                                                                   
-int add(int a, int b)                                                                                                                                                 
-{                                                                                                                                                                     
-        return (a + b);                                                                                                                                               
+/**
+ * add - Add 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+        return (a + b);
 }
 
-/**                                                                                                                                                                   
- * sub - Subtract 2 numbers                                                                                                                                                
- * @a: num 1                                                                                                                                                          
- * @b: num 2                                                                                                                                                          
- * Return: subtraction of a and b                                                                                                                                            
- */                                                                                                                                               
-int add(int a, int b)                                                                                                                                                 
-{                                                                                                                                                                     
-        return (a + b);                                                                                                                                               
+
+/**
+ * sub - Subtract 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: a - b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
 }
 
-/**                                                                                                                                                                   
- * add - Add 2 numbers                                                                                                                                                
- * @a: num 1                                                                                                                                                          
- * @b: num 2                                                                                                                                                          
- * Return: sum of a and b                                                                                                                                             
- */                                                                                                                                                                   
-int add(int a, int b)                                                                                                                                                 
-{                                                                                                                                                                     
-        return (a + b);                                                                                                                                               
+
+/**
+ * mul - multiply 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+        return (a * b);
 }
 
-/**                                                                                                                                                                   
- * add - Add 2 numbers                                                                                                                                                
- * @a: num 1                                                                                                                                                          
- * @b: num 2                                                                                                                                                          
- * Return: sum of a and b                                                                                                                                             
- */                                                                                                                                                                   
-int add(int a, int b)                                                                                                                                                 
-{                                                                                                                                                                     
-        return (a + b);                                                                                                                                               
+
+/**
+ * div - Divide 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: a / b
+ */
+
+int div(int a, int b)
+{
+        return (a / b);
 }
 
-/**                                                                                                                                                                   
- * add - Add 2 numbers                                                                                                                                                
- * @a: num 1                                                                                                                                                          
- * @b: num 2                                                                                                                                                          
- * Return: sum of a and b                                                                                                                                             
- */                                                                                                                                                                   
-int add(int a, int b)                                                                                                                                                 
-{                                                                                                                                                                     
-        return (a + b);                                                                                                                                               
+
+/**
+ * mod - Find remainder after dividing 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+        return (a % b);
 }
