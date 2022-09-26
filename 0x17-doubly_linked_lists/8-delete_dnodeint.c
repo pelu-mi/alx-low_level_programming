b1b5beb0eb63e5deab22e7efee492162c36d15d8
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-26 10:49:59 +0100

    Fix 1 for task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,9 +12,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *node = *head, *p, *n;
 
+	if (node == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		n = node->next;
+		if (n == NULL)
+		{
+			*head = NULL;
+			return (1);
+		}
 		n->prev = NULL;
 		*head = node->next;
 		return (1);
