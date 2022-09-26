54d8c989461326d79125877ba821f78ca19007c7
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-26 18:04:41 +0100

    Added file for task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget 
-export LD_PRELOAD=
+wget -P /tmp/ https://github.com/pelu-mi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/make-me-win.so
+export LD_PRELOAD=/tmp/make-me-win.so
